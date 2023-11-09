6293ed7fac0e45d539b7bb88174e3fcb5aa6e72d
Jesus13vr <2019452150@teschi.edu.mx>
2023-11-08 19:58:15 -0600

    comit

--- APIPRUEBA/APIPRUEBA/settings.py
+++ APIPRUEBA/APIPRUEBA/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'white'
+    'whitenoise.middleware.WhiteNoiseMiddleawer',
 ]
 
 ROOT_URLCONF = 'APIPRUEBA.urls'
@@ -134,7 +134,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+if not DEBUG:
+    STATIC_ROOT= os.path.join(BASE_DIR,'staticfiles')
+    STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
