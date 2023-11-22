4a73c5f84d9904a4ec29a87d6eb68eb759aab633
Jesus13vr <2019452150@teschi.edu.mx>
2023-11-21 19:12:37 -0600

    clase_oscar

--- APIPRUEBA/api/models.py
+++ APIPRUEBA/api/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 class usuario(models.Model):
     idUsu=models.CharField(max_length=20,primary_key=True, db_column='idCliente')
@@ -22,3 +22,50 @@ class Encuestadesatisfaccion(models.Model):
     p10=models.TextField(db_column='Cuál es tu grado de satisfacción general con el proyecto de administración')
     class Meta:
         db_table = 'Encuestadesatisfaccion'
+
+
+class Rol(models.Model):
+    id_Rol = models.AutoField(primary_key=True)
+    Descripción = models.CharField(max_length=45)
+
+    class Meta:
+        db_table = 'Rol'
+
+class Jefa(models.Model):
+    id_Jefa = models.AutoField(primary_key=True)
+    Rol_id_Rol = models.ForeignKey(Rol, on_delete=models.DO_NOTHING)
+    usuario= models.ForeignKey(User,on_delete=models.CASCADE)
+    class Meta:
+        db_table = 'Jefa'
+
+class Alumno(models.Model):
+    idAlumno = models.AutoField(primary_key=True)
+    Rol_id_Rol = models.ForeignKey(Rol, on_delete=models.DO_NOTHING)
+    usuario= models.ForeignKey(User,on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'Alumno'
+
+class Materia(models.Model):
+    idMateria = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=45)
+    semestre = models.CharField(max_length=45)
+    creditos = models.IntegerField()
+
+    class Meta:
+        db_table = 'Materia'
+
+class Seguimiento(models.Model):
+    idSeguimiento = models.AutoField(primary_key=True)
+    parcial = models.CharField(max_length=45)
+
+    class Meta:
+        db_table = 'Seguimiento'
+
+class Docente(models.Model):
+    idDocente = models.AutoField(primary_key=True)
+    Rol_id_Rol = models.ForeignKey(Rol, on_delete=models.DO_NOTHING)
+    usuario= models.ForeignKey(User,on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'Docente'
