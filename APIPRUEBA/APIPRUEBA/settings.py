f9b9694c70d73430e67a8be4d13130b38c7c319e
Jesus13vr <2019452150@teschi.edu.mx>
2023-12-04 23:42:32 -0600

    pc

--- APIPRUEBA/APIPRUEBA/settings.py
+++ APIPRUEBA/APIPRUEBA/settings.py
@@ -139,7 +139,7 @@ STATIC_URL = 'https://storage.googleapis.com/lic_admon/static/'
 # Usar Google Cloud Storage para almacenar archivos de medios.
 DEFAULT_FILE_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
 GS_BUCKET_NAME = 'lic_admon'
-
+ f
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
